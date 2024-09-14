30cd9b3012b741c8e81d8cb9346d7545c5b94c76
ftnext <takuyafjp+develop@gmail.com>
2024-09-14 15:35:11 +0900

    [feat] Run mylib
    - uv run python -m mylib
    - .venv/bin/python -m mylib

---
+++ mylib/src/mylib/__main__.py
@@ -0,0 +1,12 @@
+from the_solitary_castle_in_the_mirror import characters
+
+from mylib import hello
+
+
+def main():
+    print(hello())
+    print(characters)
+
+
+if __name__ == "__main__":
+    main()
