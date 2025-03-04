4c463d197451c1a81ff2fce32ebf886c0fc374da
ftnext <takuyafjp+develop@gmail.com>
2025-03-05 08:44:53 +0900

    [feat] Sample FastAPI application

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from fastapi-app!")
+from fastapi import FastAPI
+from the_solitary_castle_in_the_mirror import characters
+from the_solitary_castle_in_the_mirror.__main__ import _main
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def main():
+    return _main(characters)
